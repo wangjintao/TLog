0677b93292e4d4835aafc535a8456ba10171be44
wangjintao <wangjintao1988@163.com>
2026-04-24 15:35:45 +0800

    添加长日志打印支持

--- sample/src/main/java/com/tao/sample/MainActivity.kt
+++ sample/src/main/java/com/tao/sample/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.tao.sample
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.tao.sample.databinding.ActivityMainBinding
 import com.tao.tlog.v2.TLog
 
@@ -18,9 +15,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(mViewBinding.root)
 
         mViewBinding.addLogBtn.setOnClickListener {
-            TLog.d("测试日志")
+            val poem = buildLongPoemLog()
+            TLog.d(poem)
         }
+    }
 
+    private fun buildLongPoemLog(): String {
+        val poem = """
+            《行路难》
+            作者：李白
+            金樽清酒斗十千，玉盘珍羞直万钱。
+            停杯投箸不能食，拔剑四顾心茫然。
+            欲渡黄河冰塞川，将登太行雪满山。
+            闲来垂钓碧溪上，忽复乘舟梦日边。
+            行路难，行路难，多歧路，今安在？
+            长风破浪会有时，直挂云帆济沧海。
+        """.trimIndent()
 
+        return buildString {
+            appendLine("长日志切片输出测试开始")
+            repeat(20) { index ->
+                appendLine("第${index + 1}遍")
+                appendLine(poem)
+            }
+            append("长日志切片输出测试结束")
+        }
     }
 }
--- tlog-v2/src/main/java/com/tao/tlog/v2/printers/ConsolePrinter.kt
+++ tlog-v2/src/main/java/com/tao/tlog/v2/printers/ConsolePrinter.kt
@@ -3,19 +3,79 @@ package com.tao.tlog.v2.printers
 import android.util.Log
 import com.tao.tlog.v2.constants.LogLevel
 import com.tao.tlog.v2.interfaces.Printer
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 /**
  *Author: WangJintao
  * Date: 2026/4/14 10:25
  **/
-class ConsolePrinter: Printer {
+class ConsolePrinter : Printer {
+    companion object {
+        private const val MAX_LOG_LENGTH = 3000
+        private val logExecutor: ExecutorService = Executors.newSingleThreadExecutor { runnable ->
+            Thread(runnable, "TLog-Console").apply {
+                isDaemon = true
+            }
+        }
+    }
+
     override fun println(level: Int, tag: String, msg: String) {
-        when (level) {
-            LogLevel.VERBOSE -> Log.v(tag, msg)
-            LogLevel.DEBUG -> Log.d(tag, msg)
-            LogLevel.INFO -> Log.i(tag, msg)
-            LogLevel.WARN -> Log.w(tag, msg)
-            LogLevel.ERROR -> Log.e(tag, msg)
+        logExecutor.execute {
+            printInternal(level, tag, msg)
+        }
+    }
+
+    private fun printInternal(level: Int, tag: String, msg: String) {
+        if (msg.isEmpty()) {
+            printChunk(level, tag, msg)
+            return
+        }
+
+        if (msg.length <= MAX_LOG_LENGTH && msg.indexOf('\n') == -1) {
+            printChunk(level, tag, msg)
+            return
+        }
+
+        val length = msg.length
+        var lineStart = 0
+        while (lineStart < length) {
+            val lineEnd = msg.indexOf('\n', lineStart).let { if (it == -1) length else it }
+            printRange(level, tag, msg, lineStart, lineEnd)
+            lineStart = lineEnd + 1
+        }
+
+        if (msg[length - 1] == '\n') {
+            printChunk(level, tag, "")
+        }
+    }
+
+    private fun printRange(level: Int, tag: String, msg: String, start: Int, end: Int) {
+        if (start >= end) {
+            printChunk(level, tag, "")
+            return
+        }
+
+        var chunkStart = start
+        while (chunkStart < end) {
+            val chunkEnd = minOf(chunkStart + MAX_LOG_LENGTH, end)
+            printChunk(level, tag, msg.substring(chunkStart, chunkEnd))
+            chunkStart = chunkEnd
+        }
+    }
+
+    private fun printChunk(level: Int, tag: String, msg: String) {
+        Log.println(toAndroidPriority(level), tag, msg)
+    }
+
+    private fun toAndroidPriority(level: Int): Int {
+        return when (level) {
+            LogLevel.VERBOSE -> Log.VERBOSE
+            LogLevel.DEBUG -> Log.DEBUG
+            LogLevel.INFO -> Log.INFO
+            LogLevel.WARN -> Log.WARN
+            LogLevel.ERROR -> Log.ERROR
+            else -> Log.DEBUG
         }
     }
 }
